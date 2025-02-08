4166653c3c1dc2bca15b18ea1b7d01d8e4a903c7
Saket Chaturvedi <82042067+schatur2@users.noreply.github.com>
2025-02-08 11:55:14 -0500

    # updated codes

--- Global_Local_Attention_Project/TFRecord_Codes/tools_CGL_fusion/CreateTFRecords/tf_ssd.sh
+++
@@ -1,3 +0,0 @@
-python create_generic_db2_4c_bg_db_n.py -r /data/datasets/saket/SeeingThroughFogData -d /data/datasets/saket -id SeeingThroughFogData -f /home/saket/Dense/splits/new/test_clear_night1.txt -s test_clear_camera_gated_lidar_night_4c_1_ssd
-
-python create_generic_db2_4c_bg_db_n.py -r /data/datasets/saket/SeeingThroughFogData -d /data/datasets/saket -id SeeingThroughFogData -f /home/saket/Dense/splits/new/test_clear_night2.txt -s test_clear_camera_gated_lidar_night_4c_2_ssd
--- Global_Local_Attention_Project/TFRecord_Codes/tools_CGL_fusion/CreateTFRecords/tp.sh
+++
@@ -1,2 +0,0 @@
-
-while ps -p 46384 > /dev/null; do sleep 1; done; tf.sh
